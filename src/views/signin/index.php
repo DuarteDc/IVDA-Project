1beb9d76f0a025c1725f6c8bf5af699d3dd8b8b1
DuarteDc <al221910409@gmail.com>
2024-01-19 15:10:11 -0600

    production

--- src/views/signin/index.php
+++ src/views/signin/index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="/vite.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
     <title>IVDA</title>
-    <script type="module" crossorigin src="/assets/index-7ed2f9ff.js"></script>
-    <link rel="stylesheet" href="/assets/index-c581d846.css">
+    <script type="module" crossorigin src="/assets/index-8d7bc033.js"></script>
+    <link rel="stylesheet" href="/assets/index-31771798.css">
   </head>
   <body class="text-foreground bg-background">
     <div id="root"></div>
